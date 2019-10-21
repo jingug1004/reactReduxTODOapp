9119b80e40d1ca123d36096b2d2e21cfc3230c53
jingug1004 <jingug1004@gmail.com>
2019-10-21 21:20:48 +0900

    191021(월)_리덕스 상태

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,4 @@
-import {tsExternalModuleReference} from "@babel/types";
+import {exportAllDeclaration, tsExternalModuleReference} from "@babel/types";
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -30,3 +30,52 @@ export const remove = id => ({
     type: REMOVE,
     id
 });
+
+const initialState = {
+    input: '',
+    todos: [
+        {
+            id: 1,
+            text: '리덕스 기초 배우기',
+            done: true
+        },
+        {
+            id: 2,
+            text: '리액트와 리덕스 사용하기',
+            done: false
+        }
+    ]
+};
+
+function todos(state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_INPUT:
+            return {
+                ...state,
+                input: action.input
+            };
+        case INSERT:
+            return {
+                ...state,
+                todos: state.todos.concat(action.todo)
+            };
+        case TOGGLE:
+            return {
+                ...state,
+                todos: state.todos.map(todo =>
+                    todo.id === action.id ? {
+                        ...todo, done: !todo.done
+                    } : todo
+                )
+            };
+        case REMOVE:
+            return {
+                ...state,
+                todos: state.todos.filter(todo => todo.id !== action.id)
+            };
+        default:
+            return this.state;
+    }
+}
+
+export default todos;
